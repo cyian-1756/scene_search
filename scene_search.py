6057dcbce6e46f8a76145457f2234fb0346efaaa
cyian-1756 <devnull64@vfemail.net>
2016-05-21 23:07:15 -0400

    Now has a basic gui

--- scene_search.py
+++ scene_search.py
@@ -6,13 +6,21 @@ parser.add_argument("-n", help="Name of the porn star you're searching for")
 parser.add_argument("-s", help="Name of the scene you're searching for")
 args = parser.parse_args()
 
-name_list = args.n.split()
-
-scene_list = args.s.split()
+if args.n and args.s:
+    name_whole = args.n
+    scene_long = args.s
+    name_list = args.n.split()
+    scene_list = args.s.split()
+else:
+    import easygui
+    name_whole = easygui.enterbox('Pornstar name', 'Scene Search')
+    scene_long = easygui.enterbox('Scene name', 'Scene Search')
+    name_list = name_whole.split()
+    scene_list = scene_long.split()
 
 name_first = name_list[0]
 name_last = name_list[1]
-name_whole = args.n
+
 
 def get_scene_short():
     name = ''
@@ -20,12 +28,15 @@ def get_scene_short():
         name = name + i[0]
     return name
 
-scene_long = args.s
+
 scene_short = get_scene_short()
 
-print('{0} {1}'.format(name_whole, scene_long))
-print('{0} {1}'.format(name_first, scene_long))
-print('{0} {1}'.format(name_last, scene_long))
-print('{0} {1}'.format(name_whole, scene_short))
-print('{0} {1}'.format(name_first, scene_short))
-print('{0} {1}'.format(name_last, scene_short))
+if args.s and args.n:
+    print('{0} {1}'.format(name_whole, scene_long))
+    print('{0} {1}'.format(name_first, scene_long))
+    print('{0} {1}'.format(name_last, scene_long))
+    print('{0} {1}'.format(name_whole, scene_short))
+    print('{0} {1}'.format(name_first, scene_short))
+    print('{0} {1}'.format(name_last, scene_short))
+else:
+    easygui.msgbox('{0} {1}\n{2} {3}\n{4} {5}\n{6} {7}\n{8} {9}\n{10} {11}'.format(name_whole, scene_long, name_first, scene_long, name_last, scene_long, name_whole, scene_short, name_first, scene_short, name_last, scene_short), 'Scene Search')
